2e54aeb74a54fdd71362c80155746a1e3cefcf95
Frederik Weber <frederikweber.93@gmail.com>
2017-11-05 15:03:29 +0100

    Upgraded AppDelegate and Storyboard to Swift 4

--- Example/Example/Example/AppDelegate.swift
+++ Example/Example/Example/AppDelegate.swift
@@ -12,10 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
 
